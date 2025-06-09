0f465f9ccf85270a0ffa68bbf0e7231f81fea05a
eslam148 <islam.18333388@comit.aun.edu.eg>
2025-06-09 20:24:55 +0300

    fix guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,6 +3,7 @@ import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, map, take } from 'rxjs';
 import { selectAuthResponse } from '../store/auth/auth.selectors';
+import { Roles } from '../Enums/Roles.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,17 @@ export class AuthGuard implements CanActivate {
     return this.store.select(selectAuthResponse).pipe(
       take(1),
       map(response => {
-        if (response?.token) {
+        console.log("response from auth guard", response);
+        if (response?.token && response?.role === Roles.Admin) {
           return true;
+        }
+        else{
+          localStorage.removeItem('authToken');
+          localStorage.removeItem('user');
+          localStorage.removeItem('fcmTokenData');
+          localStorage.removeItem('token');
+          localStorage.removeItem('fcmToken');
+          localStorage.removeItem('fcmTokenData');
         }
         return this.router.createUrlTree(['/login']);
       })
