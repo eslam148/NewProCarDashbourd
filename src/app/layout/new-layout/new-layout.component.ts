914dff45d30b845aaf728793bd699c867c0e550b
eslam148 <islam.18333388@comit.aun.edu.eg>
2025-06-09 15:22:12 +0300

    add translation to landing page

--- src/app/layout/new-layout/new-layout.component.ts
+++ src/app/layout/new-layout/new-layout.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslatePipe } from '../../pipes/translate.pipe';
 
 @Component({
   selector: 'app-new-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslatePipe],
   templateUrl: './new-layout.component.html',
   styleUrls: ['./new-layout.component.scss']
 })
 export class NewLayoutComponent {
-  // Add any component logic here
+  currentLang: string = 'ar'; // Default to Arabic since the template is in Arabic
+
+  switchLanguage(lang: 'en' | 'ar') {
+    this.currentLang = lang;
+  }
 }
