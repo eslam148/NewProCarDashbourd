103b50d1d14cb2a38bef9d591d6ddad3d624ec15
eslam148 <islam.18333388@comit.aun.edu.eg>
2025-06-09 16:44:27 +0300

    add animation

--- src/app/layout/new-layout/new-layout.component.ts
+++ src/app/layout/new-layout/new-layout.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslatePipe } from '../../pipes/translate.pipe';
 import { LanguageSwitcherComponent } from '../../components/language-switcher/language-switcher.component';
+
 @Component({
   selector: 'app-new-layout',
   standalone: true,
@@ -12,8 +13,21 @@ import { LanguageSwitcherComponent } from '../../components/language-switcher/la
 })
 export class NewLayoutComponent {
   currentLang: string = 'ar'; // Default to Arabic since the template is in Arabic
+  activeFaqIndex: number | null = null;
 
   switchLanguage(lang: 'en' | 'ar') {
     this.currentLang = lang;
+  }
+
+  toggleFaq(index: number) {
+    if (this.activeFaqIndex === index) {
+      this.activeFaqIndex = null;
+    } else {
+      this.activeFaqIndex = index;
+    }
+  }
+
+  isFaqActive(index: number): boolean {
+    return this.activeFaqIndex === index;
   }
 }
