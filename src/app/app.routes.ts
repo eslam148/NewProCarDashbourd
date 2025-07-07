b37173d6e64b7e4b6a5af11ed7224979f64fa40a
sabreen01 <sabreen.20377412@compit.aun.edu.eg>
2025-07-07 15:48:17 +0200

    split landing page and admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-  {
+  /*{
     path: '',
     component: NewLayoutComponent,
     children: [
@@ -19,7 +19,7 @@ export const routes: Routes = [
         }
       }
     ]
-  },
+  },*/
   {
     path: '',
     redirectTo: 'profile',
